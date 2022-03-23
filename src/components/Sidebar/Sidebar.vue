7d162bc0bff47a722d637f67d45ecd8e684d7ae4
TerraDev64 <TerraDev64@gmail.com>
2022-03-23 15:19:22 +0100

    sidebar WIP

---
+++ src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="sidebar">Sidebar</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Sidebar',
+  setup() {
+    
+  },
+})
+</script>
+
+<style lang="scss">
+  .sidebar {
+    background-color: black;
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Sidebar class=" fixed left-0 top-0 bottom-0 w-1/5"/>
+    <div class="body">
+      <img alt="Vue logo" src="../assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Sidebar from '@/components/Sidebar/Sidebar.vue';
 
 export default defineComponent({
   name: 'HomeView',
   components: {
     HelloWorld,
+    Sidebar,
   },
 });
 </script>
+
+<style lang="scss">
+  .body {
+    margin-left: 20%;
+  }
+</style>
